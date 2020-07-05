4c4d535cdd333ba87741947f300857cab375a4f4
dexter264 <67840067+dexter264@users.noreply.github.com>
2020-07-05 17:30:58 -0400

    Added shooting and made dat sun not so painfull
    i guess it wasnt THAT painfull though:(

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 5f);
+    }
+    void Update()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform gunTip;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletPrefab, gunTip.transform.position, gunTip.transform.rotation);
+        }
+    }
+}
